47712f7a8d79927b4850d2ef4ca199be693d7336
Oliver <810982048@qq.com>
2018-12-09 20:25:33 +0800

    我的博客项目后端搭建

---
+++ app/controller/home.js
@@ -0,0 +1,11 @@
+// app/controller/home.js
+
+const Controller = require('egg').Controller;
+
+class HomeController extends Controller {
+  async index() {
+    this.ctx.body = 'Hello world';
+  }
+}
+
+module.exports = HomeController;
